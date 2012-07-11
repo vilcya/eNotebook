f46adb0370a9c8c45fbe15edf2b0d1365f7e15ec
Vilcya Wirantana <vwirantana@gmail.com>
2012-07-11 14:41:50 -0400

    changed edaily

--- gen/com/eNotebook/SATE2012/R.java
+++ gen/com/eNotebook/SATE2012/R.java
@@ -21,14 +21,18 @@ public final class R {
         public static final int stars=0x7f020002;
     }
     public static final class id {
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090005;
         public static final int eDailyButton=0x7f090000;
         public static final int eTecNoteButton=0x7f090001;
-        public static final int menu_settings=0x7f090003;
+        public static final int menu_settings=0x7f090006;
+        public static final int textView1=0x7f090003;
         public static final int twofiftysevenButton=0x7f090002;
     }
     public static final class layout {
         public static final int edaily=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int menu=0x7f030001;
+        public static final int splash=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
--- src/com/eNotebook/SATE2012/eDailies.java
+++ src/com/eNotebook/SATE2012/eDailies.java
@@ -8,6 +8,7 @@ public class eDailies extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.edaily);
 	}
 	
 }
