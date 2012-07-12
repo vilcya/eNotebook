c8621bd2f0bcc79830379f919c5e69e83a901a6c
Vilcya Wirantana <vwirantana@gmail.com>
2012-07-12 09:46:22 -0400

    Added back deleted files

--- src/com/eNotebook/SATE2012/Menu.java
+++ src/com/eNotebook/SATE2012/Menu.java
@@ -1,13 +1,13 @@
 package com.eNotebook.SATE2012;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Menu extends ListActivity implements View.OnClickListener{
+public class Menu extends Activity implements View.OnClickListener{
     String documents[] = {"eDailies", "eTecNotes", "257"}; 
     String name;
     
