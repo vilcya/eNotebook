48c6b155f4614a608b90990fc09cf7713dc0a1bf
Vilcya Wirantana <vwirantana@gmail.com>
2012-07-12 11:21:20 -0400

    added template

--- gen/com/eNotebook/SATE2012/R.java
+++ gen/com/eNotebook/SATE2012/R.java
@@ -16,23 +16,26 @@ public final class R {
         public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int stars=0x7f020002;
+        public static final int edaily_template=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int stars=0x7f020003;
     }
     public static final class id {
-        public static final int b257=0x7f090005;
+        public static final int b257=0x7f090006;
         public static final int bAdd=0x7f090001;
-        public static final int bEDaily=0x7f090003;
-        public static final int bETecNotes=0x7f090004;
-        public static final int menu_settings=0x7f090006;
-        public static final int tvMenuIntro=0x7f090002;
+        public static final int bEDaily=0x7f090004;
+        public static final int bETecNotes=0x7f090005;
+        public static final int imageView1=0x7f090002;
+        public static final int menu_settings=0x7f090007;
+        public static final int tvMenuIntro=0x7f090003;
         public static final int tvedaily=0x7f090000;
     }
     public static final class layout {
         public static final int edaily=0x7f030000;
-        public static final int menu=0x7f030001;
-        public static final int splash=0x7f030002;
+        public static final int edailyedit=0x7f030001;
+        public static final int menu=0x7f030002;
+        public static final int splash=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
