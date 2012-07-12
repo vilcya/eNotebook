2dee92c7009962633d00f752a3f1a80fb39d1a13
Kreme <maulik2009@my.fit.edu>
2012-07-12 10:51:57 -0400

    Button

--- gen/com/eNotebook/SATE2012/R.java
+++ gen/com/eNotebook/SATE2012/R.java
@@ -21,11 +21,12 @@ public final class R {
         public static final int stars=0x7f020002;
     }
     public static final class id {
-        public static final int b257=0x7f090004;
-        public static final int bEDaily=0x7f090002;
-        public static final int bETecNotes=0x7f090003;
-        public static final int menu_settings=0x7f090005;
-        public static final int tvMenuIntro=0x7f090001;
+        public static final int b257=0x7f090005;
+        public static final int bAdd=0x7f090001;
+        public static final int bEDaily=0x7f090003;
+        public static final int bETecNotes=0x7f090004;
+        public static final int menu_settings=0x7f090006;
+        public static final int tvMenuIntro=0x7f090002;
         public static final int tvedaily=0x7f090000;
     }
     public static final class layout {
@@ -43,12 +44,11 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060001;
         public static final int menu_settings=0x7f060002;
-        public static final int s257=0x7f060008;
-        public static final int sDaily=0x7f060006;
-        public static final int sMenu=0x7f060004;
-        public static final int sSplash=0x7f060005;
-        public static final int sTecNotes=0x7f060007;
-        public static final int title_activity_main=0x7f060003;
+        public static final int s257=0x7f060007;
+        public static final int sDaily=0x7f060005;
+        public static final int sMenu=0x7f060003;
+        public static final int sSplash=0x7f060004;
+        public static final int sTecNotes=0x7f060006;
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
