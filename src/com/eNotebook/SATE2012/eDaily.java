6c2d5cfc1989bb1278fdb362c84d9bda0a489da2
Kreme <maulik2009@my.fit.edu>
2012-07-16 09:12:24 -0400

    none

--- src/com/eNotebook/SATE2012/eDaily.java
+++ src/com/eNotebook/SATE2012/eDaily.java
@@ -6,16 +6,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class eDaily extends Activity implements View.OnClickListener{
 
 	Button save;
+	Button back;
+	TextView text;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +29,55 @@ public class eDaily extends Activity implements View.OnClickListener{
 		assignedObjects();
 		
 		save.setOnClickListener(this);
+		back.setOnClickListener(this);
 	}
-	
 	
 	private void assignedObjects()
 	{
 		save = (Button) findViewById(R.id.bSave);
+		back = (Button) findViewById(R.id.bEDailyBack);
+		text = (TextView) findViewById(R.id.tvDisplayPath);
 	}
+	
 	
 	public void onClick(View view)
 	{
-		// Create a bitmap out of the view
-		Bitmap neweDaily = findViewById(R.id.leDaily).getDrawingCache();
+		if (view.getId() == R.id.bEDailyBack){
+        	Intent ourIntent = new Intent("com.eNotebook.SATE2012." + "EDAILYMENU");
+        	startActivity(ourIntent);
+        }
 		
-		/* Find the path to save and create a directory if one
-		   does not exist */
-		File edailypath = new File(getFilesDir(), "eDailies");
-		if (!edailypath.exists())
-			edailypath.mkdir();
-
-		// Create a new file for the new eDaily
-		File newedaily = new File(edailypath, "test1.png");
-		try { newedaily.createNewFile(); }
-		catch(IOException e) 
-		{ e.printStackTrace(); }
-		
-		// Open the file stream and copy the image into the file
-		try
-		{
-			TextView dummytxt = (TextView) findViewById(R.id.tvDisplayPath);
-			dummytxt.setText("I'm saving the picture at " + newedaily.toString());
-			FileOutputStream ostream = new FileOutputStream(newedaily);
-			neweDaily.compress(CompressFormat.PNG, 100, (OutputStream)ostream);
+		else{
+			
+			// Create a bitmap out of the view
+			Bitmap neweDaily = findViewById(R.id.leDaily).getDrawingCache();
+			
+			/* Find the path to save and create a directory if one
+			  does not exist */
+			File edailypath = new File(getFilesDir(), "eDailies");
+			if (!edailypath.exists())
+				edailypath.mkdir();
+	
+			// Create a new file for the new eDaily
+			File newedaily = new File(edailypath, "test1.png");
+			try { newedaily.createNewFile(); }
+			catch(IOException e) 
+			{ e.printStackTrace(); }
+			
+			// Open the file stream and copy the image into the file
+			try
+			{
+				TextView dummytxt = (TextView) findViewById(R.id.tvDisplayPath);
+				dummytxt.setText("I'm saving the picture at " + newedaily.toString());
+				FileOutputStream ostream = new FileOutputStream(newedaily);
+				neweDaily.compress(CompressFormat.PNG, 100, (OutputStream)ostream);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
 		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		
 	}
 	
 	
