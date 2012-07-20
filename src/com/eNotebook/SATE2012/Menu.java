d7f63dadb705ea818b54e8391f36bb264b56924d
Kreme <maulik2009@my.fit.edu>
2012-07-20 08:53:29 -0400

    New Preview Format

--- src/com/eNotebook/SATE2012/Menu.java
+++ src/com/eNotebook/SATE2012/Menu.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Menu extends Activity implements View.OnClickListener{
     String documents[] = {"eDailyMenu", "eTecNote", "257", "Option"}; 
